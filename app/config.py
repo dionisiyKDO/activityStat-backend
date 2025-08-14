92e867fc15050bcc0fcfebc98c1ce1d924e67d61
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-15 01:03:25 +0300

    config file with paths

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# app/config.py
+import os
+
+data_path = os.path.join("app", "data", "export")
+
+title_map_path = os.path.join("app", "data", "app_title_map.json")
+
+database_name = "data.db"
+database_path = os.path.join("app", "data", database_name)
