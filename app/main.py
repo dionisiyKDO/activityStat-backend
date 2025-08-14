963e2be21be6db0807dc1a453e6509db1d3a6319
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-14 20:30:16 +0300

    init db on app start

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from utils import get_app_list, get_spent_time, get_daily_app_usage, get_dataset_metadata, create_app_title_mapping
+from utils import get_app_list, get_spent_time, get_daily_app_usage, get_dataset_metadata, create_app_title_mapping, init_db
 
 app = FastAPI()
 
@@ -38,5 +38,6 @@ def dataset_metadata():
 
 
 if __name__ == "__main__":
+    init_db()
     create_app_title_mapping()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
