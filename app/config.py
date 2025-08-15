03fdf14996de49515ff8a3fcb6ba627ef390d94b
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-15 04:03:28 +0300

    change map variables

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ import os
 
 data_path = os.path.join("app", "data", "export")
 
-title_map_path = os.path.join("app", "data", "app_title_map.json")
+flatten_apps_to_title_map = os.path.join("app", "data", "flatten_apps_to_title_map.json")
+flatten_title_to_apps_map = os.path.join("app", "data", "flatten_title_to_apps_map.json")
 
 database_name = "data.db"
 database_path = os.path.join("app", "data", database_name)
